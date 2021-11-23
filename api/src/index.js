e6dbc4b50fd764ebe45bff0a445f7e65f3e1c54d
jilink <jimmyfac1@gmail.com>
2021-11-23 15:19:36 +0100

    Auto generate constraint when using unique directive

--- api/src/index.js
+++ api/src/index.js
@@ -49,35 +49,27 @@ const neoSchema = new Neo4jGraphQL({
     database: process.env.NEO4J_DATABASE || 'neo4j',
   },
 })
+neoSchema
+  .assertIndexesAndConstraints({ options: { create: true } })
+  .then(() => {
+    const server = new ApolloServer({
+      context: ({ req }) => ({
+        req,
+        driver,
+        driverConfig: { database: process.env.NEO4J_DATABASE || 'neo4j' },
+      }),
+      schema: neoSchema.schema,
+      introspection: true,
+      playground: true,
+    })
 
-/*
- * Create a new ApolloServer instance, serving the GraphQL schema
- * created using makeAugmentedSchema above and injecting the Neo4j driver
- * instance into the context object so it is available in the
- * generated resolvers to connect to the database.
- */
-const server = new ApolloServer({
-  context: ({ req }) => ({
-    req,
-    driver,
-    driverConfig: { database: process.env.NEO4J_DATABASE || 'neo4j' },
-  }),
-  schema: neoSchema.schema,
-  introspection: true,
-  playground: true,
-})
+    const port = process.env.GRAPHQL_SERVER_PORT || 4001
+    const path = process.env.GRAPHQL_SERVER_PATH || '/graphql'
+    const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0'
 
-// Specify host, port and path for GraphQL endpoint
-const port = process.env.GRAPHQL_SERVER_PORT || 4001
-const path = process.env.GRAPHQL_SERVER_PATH || '/graphql'
-const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0'
+    server.applyMiddleware({ app, path })
 
-/*
- * Optionally, apply Express middleware for authentication, etc
- * This also also allows us to specify a path for the GraphQL endpoint
- */
-server.applyMiddleware({ app, path })
-
-app.listen({ host, port, path }, () => {
-  console.log(`GraphQL server ready at http://${host}:${port}${path}`)
-})
+    app.listen({ host, port, path }, () => {
+      console.log(`GraphQL server ready at http://${host}:${port}${path}`)
+    })
+  })
