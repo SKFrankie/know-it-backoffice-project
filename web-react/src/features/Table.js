d1b20d3d622e15bdaaac2a0d7a1fd84730aa54cd
jilink <jimmyfac1@gmail.com>
2021-11-29 15:07:09 +0100

    rows per page & pagniation are now bounded to graphql

--- web-react/src/features/Table.js
+++ web-react/src/features/Table.js
@@ -23,36 +23,6 @@ import { visuallyHidden } from '@mui/utils'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1
-  }
-  return 0
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy)
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index])
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0])
-    if (order !== 0) {
-      return order
-    }
-    return a[1] - b[1]
-  })
-  return stabilizedThis.map((el) => el[0])
-}
-
 function EnhancedTableHead(props) {
   const {
     onSelectAllClick,
@@ -182,13 +152,15 @@ const Table = ({
   rows = [],
   tableName = '',
   extraColumns = null,
-  defaultLimit = 50,
+  refetch = null,
+  count,
+  limit = 50,
 }) => {
   const [order, setOrder] = React.useState('asc')
   const [orderBy, setOrderBy] = React.useState('calories')
   const [selected, setSelected] = React.useState([])
   const [page, setPage] = React.useState(0)
-  const [rowsPerPage, setRowsPerPage] = React.useState(defaultLimit)
+  const [rowsPerPage, setRowsPerPage] = React.useState(limit)
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc'
@@ -227,18 +199,17 @@ const Table = ({
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage)
+    refetch({ limit: rowsPerPage, offset: rowsPerPage * newPage })
   }
 
   const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10))
+    const tmpRowsPerPage = parseInt(event.target.value, 10)
+    setRowsPerPage(tmpRowsPerPage)
     setPage(0)
+    refetch({ limit: tmpRowsPerPage, offset: rowsPerPage * page })
   }
 
   const isSelected = (name) => selected.indexOf(name) !== -1
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -259,11 +230,8 @@ const Table = ({
               headCells={headCells}
             />
             <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-                 rows.slice().sort(getComparator(order, orderBy)) */}
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
+              {rows.length &&
+                rows.map((row, index) => {
                   return (
                     <Row
                       headCells={headCells}
@@ -276,22 +244,13 @@ const Table = ({
                     />
                   )
                 })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: 53 * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
             </TableBody>
           </MUITable>
         </TableContainer>
         <TablePagination
           rowsPerPageOptions={[1, 10, 50, 100]}
           component="div"
-          count={rows.length}
+          count={count}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
