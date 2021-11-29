98728fc2eff3205ad70713176c4be3d9bc732364
jilink <jimmyfac1@gmail.com>
2021-11-29 14:41:38 +0100

    Table component + user page Partie 1

---
+++ web-react/src/features/Table.js
@@ -0,0 +1,384 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { alpha } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Collapse from '@mui/material/Collapse'
+import MUITable from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TablePagination from '@mui/material/TablePagination'
+import TableRow from '@mui/material/TableRow'
+import TableSortLabel from '@mui/material/TableSortLabel'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import Checkbox from '@mui/material/Checkbox'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import DeleteIcon from '@mui/icons-material/Delete'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import { visuallyHidden } from '@mui/utils'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0])
+    if (order !== 0) {
+      return order
+    }
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map((el) => el[0])
+}
+
+function EnhancedTableHead(props) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+    headCells,
+  } = props
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property)
+  }
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        <TableCell />
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  )
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+  headCells: PropTypes.arrayOf(String).isRequired,
+}
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected, tableName } = props
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          {tableName}
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  )
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  tableName: PropTypes.string.isRequired,
+}
+
+const Table = ({
+  headCells = [],
+  rows = [],
+  tableName = '',
+  extraColumns = null,
+  defaultLimit = 50,
+}) => {
+  const [order, setOrder] = React.useState('asc')
+  const [orderBy, setOrderBy] = React.useState('calories')
+  const [selected, setSelected] = React.useState([])
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(defaultLimit)
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc'
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.userId)
+      setSelected(newSelecteds)
+      return
+    }
+    setSelected([])
+  }
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name)
+    let newSelected = []
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name)
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1))
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1))
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      )
+    }
+
+    setSelected(newSelected)
+  }
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
+  const isSelected = (name) => selected.indexOf(name) !== -1
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <EnhancedTableToolbar
+          numSelected={selected.length}
+          tableName={tableName}
+        />
+        <TableContainer>
+          <MUITable sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+              headCells={headCells}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  return (
+                    <Row
+                      headCells={headCells}
+                      key={index}
+                      row={row}
+                      index={index}
+                      isSelected={isSelected}
+                      handleClick={handleClick}
+                      extraColumns={extraColumns}
+                    />
+                  )
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </MUITable>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[1, 10, 50, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  )
+}
+
+const Row = ({
+  extraColumns,
+  row,
+  headCells,
+  handleClick,
+  index,
+  isSelected,
+}) => {
+  const [open, setOpen] = React.useState(false)
+  const isItemSelected = isSelected(row.userId)
+  // WARNING checkbox is not really working, maybe pass it in props?
+  const labelId = `enhanced-table-checkbox-${index}`
+
+  return (
+    <>
+      <TableRow hover tabIndex={-1} selected={isItemSelected}>
+        <TableCell padding="checkbox">
+          <Checkbox
+            onClick={(event) => handleClick(event, row.userId)}
+            color="primary"
+            aria-checked={isItemSelected}
+            checked={isItemSelected}
+            inputProps={{
+              'aria-labelledby': labelId,
+            }}
+          />
+        </TableCell>
+        {extraColumns && (
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+        )}
+        {headCells.map((headCell) => (
+          <TableCell
+            align={headCell.numeric ? 'right' : 'left'}
+            key={headCell.id}
+          >
+            {row[headCell.id]}
+          </TableCell>
+        ))}
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <MUITable size="small">
+              <TableHead>
+                <TableRow>
+                  {extraColumns.map((extraColumn) => (
+                    <TableCell key={extraColumn.id}>
+                      {extraColumn.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  {extraColumns.map((extraColumn) => (
+                    <TableCell key={extraColumn.id}>
+                      {extraColumn.child
+                        ? row[extraColumn.id]?.[extraColumn.child]
+                        : row[extraColumn.id]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableBody>
+            </MUITable>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  )
+}
+
+export default Table
--- web-react/src/pages/Users.js
+++ web-react/src/pages/Users.js
@@ -1,35 +1,99 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client'
-import { Box } from '@mui/material'
 import Loading from '../ui/Loading'
+import Table from '../features/Table'
 
 const GET_USERS = gql`
-  query Users {
-    users {
+  query Users($limit: Int, $offset: Int) {
+    users(options: { limit: $limit, offset: $offset }) {
       userId
+
       mail
       username
-      coins
-      tickets
-      isPremium
       age
       country
       lastSeen
+      daysInArow
+
       createdAt
+      isPremium
+      coins
+      tickets
+      currentAvatar {
+        name
+        picture
+      }
     }
+    usersCount
   }
 `
 const Users = () => {
-  const { data, loading, error } = useQuery(GET_USERS)
+  const defaultLimit = 1
+  const { data, loading, error, refetch } = useQuery(GET_USERS, {
+    variables: { limit: defaultLimit },
+  })
+  const columns = [
+    { id: 'mail', numeric: false, disablePadding: false, label: 'Mail' },
+    {
+      id: 'username',
+      numeric: false,
+      disablePadding: false,
+      label: 'Username',
+    },
+    { id: 'age', numeric: true, disablePadding: false, label: 'Age' },
+    { id: 'country', numeric: false, disablePadding: false, label: 'Country' },
+    {
+      id: 'lastSeen',
+      numeric: true,
+      disablePadding: false,
+      label: 'Last Seen',
+    },
+    {
+      id: 'daysInArow',
+      numeric: true,
+      disablePadding: false,
+      label: 'Days in a row',
+    },
+  ]
+
+  const extraColumns = [
+    {
+      id: 'createdAt',
+      numeric: true,
+      disablePadding: false,
+      label: 'Created At',
+    },
+    {
+      id: 'isPremium',
+      numeric: true,
+      disablePadding: false,
+      label: 'Is Premium',
+    },
+    { id: 'coins', numeric: true, disablePadding: false, label: 'Coins' },
+    { id: 'tickets', numeric: true, disablePadding: false, label: 'Tickets' },
+    {
+      id: 'currentAvatar',
+      child: 'name',
+      numeric: false,
+      disablePadding: false,
+      label: 'Current Avatar',
+    },
+  ]
   return (
-    <Box>
-      {loading && <Loading />}{' '}
-      {data &&
-        data.users.map((user, index) => (
-          <p key={index}>{user.username}</p>
-        ))}{' '}
-      {error && 'error'}
-    </Box>
+    <>
+      {loading && <Loading />} {error && 'error'}
+      {data && (
+        <Table
+          tableName="Users"
+          headCells={columns}
+          rows={data.users}
+          extraColumns={extraColumns}
+          refetch={refetch}
+          count={data.usersCount}
+          limit={defaultLimit}
+        />
+      )}
+    </>
   )
 }
 
