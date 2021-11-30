4044a366f7751566b4f56b5d662f57a2fcf296a0
jilink <jimmyfac1@gmail.com>
2021-11-30 10:51:41 +0100

    searchBar working fine with table

---
+++ web-react/src/features/SearchBar.js
@@ -0,0 +1,74 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Input } from '../ui/Form'
+
+const SearchBar = ({
+  searchFields = [{ label: 'no fields', id: 'no-fields' }],
+  refetch,
+}) => {
+  const [search, setSearch] = React.useState('')
+  const [searchBy, setSearchBy] = React.useState(searchFields[0])
+  const [menuItems, setMenuItems] = React.useState(searchFields)
+  const fieldToSearch = (field) => {
+    if (field.id === 'no-fields') return ''
+    return `${field.id}_CONTAINS`
+  }
+  useEffect(() => {
+    const tmpSearchFields = searchFields.filter(({ numeric }) => {
+      return !numeric
+    })
+
+    setMenuItems(tmpSearchFields)
+    setSearchBy(tmpSearchFields[0])
+  }, [])
+
+  useEffect(() => {
+    if (!search) {
+      refetch({ filter: null })
+      return
+    }
+    if (refetch && searchBy.id !== 'no-fields') {
+      refetch({ filter: { [fieldToSearch(searchBy)]: search } })
+    }
+  }, [search, searchBy])
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSearchBy = (e) => {
+    const tmpSearchBy = menuItems.find((field) => field.id === e.target.value)
+    setSearchBy(tmpSearchBy)
+  }
+
+  return (
+    <Box>
+      <Input
+        placeholder={`Search by ${searchBy.label}`}
+        value={search}
+        onChange={handleSearch}
+        sx={{ width: '30%' }}
+        className="search-input"
+      />
+      <FormControl>
+        <InputLabel id="select-search">Search by</InputLabel>
+        <Select
+          labelId="select-search"
+          value={searchBy.id}
+          label={searchBy.label}
+          onChange={handleSearchBy}
+          inputProps={{ 'aria-label': 'Without label' }}
+          sx={{ m: 1 }}
+        >
+          {menuItems.map((menuItem) => (
+            <MenuItem key={menuItem.id} value={menuItem.id}>
+              {menuItem.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
+
+export default SearchBar
--- web-react/src/pages/Users.js
+++ web-react/src/pages/Users.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { useQuery, gql } from '@apollo/client'
 import Loading from '../ui/Loading'
 import Table from '../features/Table'
+import { Box } from '@mui/material'
+import SearchBar from '../features/SearchBar'
 
 const GET_USERS = gql`
-  query Users($limit: Int, $offset: Int, $orderBy: [UserSort]) {
-    users(options: { limit: $limit, offset: $offset, sort: $orderBy }) {
+  query Users(
+    $limit: Int
+    $offset: Int
+    $orderBy: [UserSort]
+    $filter: UserWhere
+  ) {
+    users(
+      options: { limit: $limit, offset: $offset, sort: $orderBy }
+      where: $filter
+    ) {
       userId
 
       mail
@@ -24,14 +34,18 @@ const GET_USERS = gql`
         picture
       }
     }
-    usersCount
+    usersAggregate(where: $filter) {
+      count
+    }
   }
 `
 
 const Users = () => {
   const defaultLimit = 1
   const { data, loading, error, refetch } = useQuery(GET_USERS, {
-    variables: { limit: defaultLimit },
+    variables: {
+      limit: defaultLimit,
+    },
   })
   const columns = [
     { id: 'mail', numeric: false, disablePadding: false, label: 'Mail' },
@@ -82,18 +96,22 @@ const Users = () => {
   ]
   return (
     <>
-      {loading && <Loading />} {error && 'error'}
-      {data && (
-        <Table
-          tableName="Users"
-          headCells={columns}
-          rows={data.users}
-          extraColumns={extraColumns}
-          refetch={refetch}
-          count={data.usersCount}
-          limit={defaultLimit}
-        />
-      )}
+      <Box>
+        <SearchBar searchFields={columns} refetch={refetch} />
+        {loading && <Loading />} {error && 'error'}
+        {data && (
+          <Table
+            tableName="Users"
+            headCells={columns}
+            rows={data.users}
+            extraColumns={extraColumns}
+            refetch={refetch}
+            count={data.usersAggregate.count}
+            limit={defaultLimit}
+            hasCheckbox={false}
+          />
+        )}
+      </Box>
     </>
   )
 }
