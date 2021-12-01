96fed556a7c35f2e151e2c09a7a74a0e53f3ca06
jilink <jimmyfac1@gmail.com>
2021-12-01 14:34:53 +0100

    Popover component

---
+++ web-react/src/ui/Popover.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import MUIPopover from '@mui/material/Popover'
+import Typography from '@mui/material/Typography'
+
+const Popover = ({ text, children, ...props }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+
+  return (
+    <>
+      <div
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      >
+        {children}
+      </div>
+      <MUIPopover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: 'none',
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+        {...props}
+      >
+        <Typography sx={{ p: 1 }}>{text}</Typography>
+      </MUIPopover>
+    </>
+  )
+}
+
+export default Popover
