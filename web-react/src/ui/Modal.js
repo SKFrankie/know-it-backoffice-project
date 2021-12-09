6d59a9a14adb07a7162ea3215cdc846d3c0dddb1
jilink <jimmyfac1@gmail.com>
2021-12-09 11:30:49 +0100

    Update modal size

--- web-react/src/ui/Modal.js
+++ web-react/src/ui/Modal.js
@@ -6,7 +6,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '80%',
   bgcolor: 'background.paper',
   border: '1px solid #000',
   boxShadow: 24,
