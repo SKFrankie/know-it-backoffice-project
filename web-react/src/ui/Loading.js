6f6fe8d53a13097e3e138eea4361d17e0955fbc3
jilink <jimmyfac1@gmail.com>
2021-12-16 15:08:58 +0100

    Loading component

--- web-react/src/ui/Loading.js
+++ web-react/src/ui/Loading.js
@@ -2,7 +2,18 @@ import { CircularProgress } from '@mui/material'
 import React from 'react'
 
 const Loading = () => {
-  return <CircularProgress />
+  return (
+    <CircularProgress
+      style={{
+        position: 'absolute',
+        width: '150px',
+        height: '150px',
+        top: 'calc(50% - 150px)',
+        left: 'calc(50% - 150px)',
+        zIndex: '999',
+      }}
+    />
+  )
 }
 
 export default Loading
