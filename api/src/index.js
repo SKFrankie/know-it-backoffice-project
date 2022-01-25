0215f57ed780b2b4a49b7779ac359d39e839cd67
jilink <jimmyfac@gmail.com>
2022-01-25 12:23:55 +0100

    cors please work

--- api/src/index.js
+++ api/src/index.js
@@ -12,7 +12,14 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.KNOW_IT_URL) // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 /*
  * Create a Neo4j driver instance to connect to the database
