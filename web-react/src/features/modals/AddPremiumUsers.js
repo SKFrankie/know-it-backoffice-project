7513ed74ef8945132193c31f25f479a6fca0637c
jilink <jimmyfac@gmail.com>
2022-06-09 11:13:40 +0200

    Are you sure modal

--- web-react/src/features/modals/AddPremiumUsers.js
+++ web-react/src/features/modals/AddPremiumUsers.js
@@ -5,6 +5,7 @@ import Flex from '../../ui/Flex'
 import Modal from '../../ui/Modal'
 import { Input } from '../../ui/Form'
 import { useMutation, gql } from '@apollo/client'
+import AreYouSure from './AreYouSure'
 
 const ADD_PREMIUM_USERS = gql`
   mutation setPremiumMultipleUsers($endingDate: DateTime!, $userIds: [ID]) {
@@ -23,37 +24,47 @@ const AddPremiumUsers = ({ open, setOpen, refetch, selectedUsers }) => {
       console.log(error)
     },
   })
-  console.log('selected', selectedUsers)
   const [date, setDate] = useState(new Date())
+  const [areYouSure, setAreYouSure] = useState(false)
   return (
-    <Modal moreStyle={{ width: '50%' }} open={open} setOpen={setOpen}>
-      <Typography m={4} textAlign="center" variant="h5">
-        Select a premium subscription ending date for selected users
-      </Typography>
-      <Input
-        InputLabelProps={{
-          shrink: true,
-        }}
-        sx={{ width: '100%' }}
-        type="date"
-        onChange={(e) => {
-          setDate(e.target.value)
-        }}
-        label="Ending Date"
-      />
-      <Flex>
-        <CTAButton
-          variant="outlined"
-          onClick={() => {
-            setPremiumMultipleUsers({
-              variables: { endingDate: date, userIds: selectedUsers },
-            })
+    <>
+      <Modal moreStyle={{ width: '50%' }} open={open} setOpen={setOpen}>
+        <Typography m={4} textAlign="center" variant="h5">
+          Select a premium subscription ending date for selected users
+        </Typography>
+        <Input
+          InputLabelProps={{
+            shrink: true,
           }}
-        >
-          Confirm
-        </CTAButton>
-      </Flex>
-    </Modal>
+          sx={{ width: '100%' }}
+          type="date"
+          onChange={(e) => {
+            setDate(e.target.value)
+          }}
+          label="Ending Date"
+        />
+        <Flex>
+          <CTAButton
+            variant="outlined"
+            onClick={() => {
+              setAreYouSure(true)
+            }}
+          >
+            Confirm
+          </CTAButton>
+        </Flex>
+      </Modal>
+      <AreYouSure
+        open={areYouSure}
+        setOpen={setAreYouSure}
+        onConfirm={() => {
+          setPremiumMultipleUsers({
+            variables: { endingDate: date, userIds: selectedUsers },
+          })
+        }}
+        text={'Are you sure you want to make these users premium?'}
+      />
+    </>
   )
 }
 
